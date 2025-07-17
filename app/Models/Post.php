3a35b18ab642977eb44d315c6c41686363ee2d44
Ajay Rai <rai.invisibleweb@gmail.com>
2025-07-17 20:22:21 +0545

    feat: scope query application and auth logo

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -2,6 +2,8 @@
 
 namespace App\Models;
 
+use Carbon\Carbon;
+use Illuminate\Contracts\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
@@ -9,4 +11,14 @@ class Post extends Model
 {
     /** @use HasFactory<\Database\Factories\PostFactory> */
     use HasFactory;
+
+    public function scopePublished(Builder $query)
+    {
+        $query->where('published_at', '<=', Carbon::now());
+    }
+
+    public function scopeFeatured(Builder $query)
+    {
+        $query->where('featured', true);
+    }
 }
