9fa5bb3e67ce2ab366593cb14603ac7161989d58
Ajay Rai <rai.invisibleweb@gmail.com>
2025-07-20 23:51:53 +0545

    feat: sorting by lates and oldest

--- resources/views/livewire/post-list.blade.php
+++ resources/views/livewire/post-list.blade.php
@@ -1,8 +1,10 @@
 <div class=" px-3 lg:px-7 py-6">
     <div class="flex justify-between items-center border-b border-gray-100">
         <div class="flex items-center space-x-4 font-light ">
-            <button class="text-gray-500 py-4">Latest</button>
-            <button class="text-gray-900 py-4 border-b border-gray-700">Oldest</button>
+            <button wire:click='setSort("desc")'
+                class="{{ $sort === 'desc' ? 'text-gray-900 border-b border-gray-700' : 'text-gray-500' }} py-4">Latest</button>
+            <button wire:click='setSort("asc")'
+                class="{{ $sort === 'asc' ? 'text-gray-900 border-b border-gray-700' : 'text-gray-500' }} py-4">Oldest</button>
         </div>
     </div>
     <div class="py-4">
